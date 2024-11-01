3ee5aea248c78859f040c85ccbf536057d29758a
sho ochiai <ochi1020@gmail.com>
2024-11-01 20:24:24 +0900

    feat: show message send time.

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -4,7 +4,12 @@ export function Message(props: { value: string, key: number }) {
   const { value, key } = props;
   return (
     <li key={key} className={"message"}>
-      { value }
+      <div className={"message_text"}>
+        { value }
+      </div>
+      <div className={"message_time"}>
+        20:00
+      </div>
     </li>
   )
 }
